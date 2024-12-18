47e691dcca0b237f2ca741d27c20996882b188bf
Cooper Raposo <cooperraposo@Coopers-MBP.lan>
2024-12-18 17:59:31 -0500

    edited readme

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
+
+    basePath: '/coooper-raposo-website',
+    assetPrefix: '/coooper-raposo-website/',
     reactStrictMode: true,
 };
 
