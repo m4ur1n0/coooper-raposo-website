6cf3a8532992d2f0e190c3347510f290f32b968d
Cooper Raposo <cooperraposo@Coopers-MBP.lan>
2025-01-04 23:30:26 -0500

    fuckyouyoufuckingfuck

--- src/components/ProjectImage.js
+++ src/components/ProjectImage.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const ProjectImage = (path_to_image, alt='') => {
+const ProjectImage = (image_name, alt='') => {
   return (
     <div className='w-[80%] max-h-[70vh] object-contain flex flex-col items-start justify-center my-24'>
-        <img src={path_to_image} alt={alt || "Project Image"} />
+        <img src={`/images/${image_name}`} alt={alt || "Project Image"} />
     </div>
   )
 }
